b11ce2773f7a82e141c68de5943632584a51993f
Zezhi Shao <864453277@qq.com>
2022-09-06 15:32:32 +0800

    docs: ✏️ clean STID arch

--- basicts/archs/STID_arch/MLP.py
+++ basicts/archs/STID_arch/MLP.py
@@ -1,16 +1,19 @@
 import torch
-import torch.nn as nn
+from torch import nn
 
-class MLP_res(nn.Module):
+
+class MultiLayerPerceptron(nn.Module):
+    """Multi-Layer Perceptron with residual links."""
+
     def __init__(self, input_dim, hidden_dim) -> None:
         super().__init__()
-        self.fc1 = nn.Conv2d(in_channels=input_dim,  out_channels=hidden_dim, kernel_size=(1,1), bias=True)
-        self.fc2 = nn.Conv2d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=(1,1), bias=True)
+        self.fc1 = nn.Conv2d(in_channels=input_dim,  out_channels=hidden_dim, kernel_size=(1, 1), bias=True)
+        self.fc2 = nn.Conv2d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=(1, 1), bias=True)
         self.act = nn.ReLU()
         self.drop = nn.Dropout(p=0.15)
 
-    def forward(self, input_data:torch.Tensor) -> torch.Tensor:
-        """feed forward of MLP.
+    def forward(self, input_data: torch.Tensor) -> torch.Tensor:
+        """Feed forward of MLP.
 
         Args:
             input_data (torch.Tensor): input data with shape [B, D, N]
@@ -18,7 +21,7 @@ class MLP_res(nn.Module):
         Returns:
             torch.Tensor: latent repr
         """
-        B, D, N, _ = input_data.shape
-        hidden  = self.fc2(self.drop(self.act(self.fc1(input_data))))      # MLP
-        hidden  = hidden + input_data                           # residual
+
+        hidden = self.fc2(self.drop(self.act(self.fc1(input_data))))      # MLP
+        hidden = hidden + input_data                           # residual
         return hidden
